150880c90d23ece72ae0fda4ad14cdcedda4e17c
naushad112 <naushad@complitech.net>
2024-02-19 13:33:33 +0530

    first

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Book;
+use Illuminate\Http\Request;
+use Illuminate\Contracts\View\View;
+
+class BookController extends Controller
+{
+    public function index(): View {
+        return view('books.index', ['books' => Book::all()]);
+    }
+}
